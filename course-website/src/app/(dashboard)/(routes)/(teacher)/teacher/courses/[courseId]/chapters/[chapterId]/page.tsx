90200199f73f531e1d52fa812d5af8aa5ba26d66
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2023-12-30 22:53:07 +0530

    chapter creation heading done

--- course-website/src/app/(dashboard)/(routes)/(teacher)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ course-website/src/app/(dashboard)/(routes)/(teacher)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,8 @@
 import {auth} from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
-import { Akaya_Kanadaka } from "next/font/google";
+import  Link  from "next/link"
+import { ArrowLeft } from "lucide-react"
 
 const ChapterIdPage =  async({params}:{params: {courseId: string; chapterId: string}}) => {
     const {userId} = auth();
@@ -25,11 +26,34 @@ const ChapterIdPage =  async({params}:{params: {courseId: string; chapterId: str
         chapter.title,
         chapter.description,
         chapter.videoUrl,
-        
-    ]
+
+    ];
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+    const completionText = `(${completedFields}/${totalFields})`
     return(
-        <div>
-            Chapter Id page
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="w-full">
+                    <Link
+                    href={`/teacher/courses/${params.courseId}`}
+                    className="flex items-center text-sm hover:text-purple-700 transition mb-6"
+                    >
+                        <ArrowLeft className="h-4 w-4 mr-2"/>
+                        Back to course setup
+                    </Link>
+                    <div className="flex items-center justify-between w-full">
+                        <div className="flex flex-col gap-y-2">
+                            <h1 className="text-2xl font-medium">
+                                Chapter Creation
+                            </h1>
+                            <span className="text-sm text-purple-600">
+                                Complete all fields {completionText}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
